24dacf4af88c143216a2b327533eb7b153377856
Na3eM17 <naeembnmh@gmail.com>
2024-09-29 16:06:31 +0300

    test

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Display  from './display.jsx'
+function App() {
+
+  return (
+    <>
+      <Display></Display>
+    </>
+  )
+}
+
+export default App
